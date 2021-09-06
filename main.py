15575d83e8bb9bf8f01e53abc42068b69d19ea76
Luke G <lagodw@gmail.com>
2021-09-05 22:44:43 -0400

    working request

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
@@ -11,41 +12,27 @@ def home():
         subreddit = request.form['subreddit']
         title = request.form['title']
         prompt = request.form['prompt']
-    else:
-        subreddit = 'blank'
-        title = 'blank'
-        prompt = 'blank'
-    return render_template('index.html', subreddit = subreddit, title = title, prompt = prompt)
-    # if celsius:
-    #     fahrenheit = fahrenheit_from(celsius)
-    # else:
-    #     fahrenheit = ""
-    # return (
-        # """<form action="" method="get">
-        #         Subreddit: <input type="text" name="subreddit">
-        #         <br>
-        #         <br>
-        #         Thread Title: <input type="text" name="title">
-        #         <br>
-        #         <br>
-        #         Prompt: <input type="text" name="prompt">
-        #         <br>
-        #         <br>
-        #         <input type="submit" value="Generate">
-        #     </form>"""
-    #     + "Fahrenheit: "
-    #     + fahrenheit
-    #     + test
-    # )
 
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
+        data = {'subreddit': subreddit,
+                'title': title,
+                'prompt': prompt
+                }
+        
+        receive = requests.post("https://api.ubiops.com/v2.1/projects/redditbot/deployments/redditbot/versions/v1/requests", 
+        json = data, 
+        headers={'Authorization':'Token 6f01efb1d2ffe93703e952d604b03862fccdcdc9'})
+
+        length_title = len(subreddit + '. ' + title + '. ')
+
+        output = receive.json()['result']['thread'][length_title:]
+
+    else:
+        subreddit = 'askreddit'
+        title = 'how does this seem?'
+        prompt = 'its a test'
+        output = 'blank'
+
+    return render_template('index.html', subreddit = subreddit, title = title, prompt = prompt, output = output)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
